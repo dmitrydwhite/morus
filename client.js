7e98dd9d8391344872a065a1d36b6f89f56b0489
Dmitry White <white.dmitry@gmail.com>
2015-10-13 15:31:41 -0700

    v 0.1.0 Python functionality

--- client.js
+++ client.js
@@ -1,20 +1,19 @@
 (function () {
   if (Window && WebSocket && !Window.morus) {
 
-    Window.morus = new WebSocket('ws://localhost:3000');
+    Window.morus = new WebSocket('ws://localhost:3000/');
 
     Window.morus.onmessage = function (data) {
-      var dataArray = data ? JSON.parse(data.data) : []; 
-      var realArray = new Array(dataArray);
+      var realArray = data ? JSON.parse(data.data) : [];
       var scaleWeight = parseInt(realArray[4]) / 10;
-      var unitByte = realArray[1];
+      var unitByte = realArray[2];
       var units = '';
       var Mweight = document.getElementsByClassName('morus-weight')[0]
       var Munits = document.getElementsByClassName('morus-units')[0]
 
-      if (unitByte === 3) {
+      if (unitByte === 12) {
         units = 'lbs.';
-      } else if (unitByte === 4) {
+      } else if (unitByte === 3) {
         units = 'kg.';        
       } else {
         units = 'NA'
@@ -28,13 +27,13 @@
       // $('.morus-units').text(units);
       if (Mweight) {
         Mweight.value = scaleWeight
+        Mweight.innerHTML = scaleWeight
       }
 
       if (Munits) {
         Munits.value = units
+        Munits.innerHTML = units
       }
-
-      console.log(realArray); // This is an array of strings.  Index 4 is the weight in tenths of lbs.
     };
 
   } else {
--- exec/py_device_reader.py
+++ exec/py_device_reader.py
@@ -13,7 +13,11 @@ class Morus_Reader():
     'DISCONNECT': 'Scale disconnecting'
   }
 
+  connection = False
+
   def start(self, vendor_id, product_id, socket):
+    print('M.O.R.U.S')
+    self.connection = socket
     # first check for our target device
     devices = HID.find_all_hid_devices()
     for device in devices:
@@ -33,6 +37,7 @@ class Morus_Reader():
   def tell(self, data):
     # broadcast data to the console and the client
     print(data)
+    self.connection.write_message(data)
 
   def resume(self):
     if self.suspended:
@@ -40,23 +45,52 @@ class Morus_Reader():
 
   def found_scale(self):
     # broadcast the goodnews and begin listening
-    self.tell(self.messages['CONNECTED'])
+    # self.tell(self.messages['CONNECTED'])
     self.scale.open()
     self.scale.set_raw_data_handler(self.listen_for_weight)
 
   def couldnt_find_scale(self):
     # broadcast the bad news and close the websocket connection
-    print('ugh')
+    self.tell(self.messages['NOT_CONNECTED'])
+    # self.socket.close()
 
   def listen_for_weight(self, data):
     # listen for weight, and send the message when weight is received
     if not self.suspended and data != self.tare:
       self.tare = data
-      self.tell(data)
+      weights_array = []
+      for num in data:
+        weights_array.append(num)
+      self.tell(bytes(weights_array))
 
   def terminate(self):
     # broadcast termination message and close the websocket connection
     print('terminating')
 
-if __name__ == "__main__":
-  Morus_Reader()
+import tornado.ioloop
+import tornado.websocket
+
+from tornado.options import define, options, parse_command_line
+
+define("port", default=3000, help="run on the given port", type=int)
+
+class ScaleSocketHandler(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+  
+    def open(self):
+        print('M.O.R.U.S')
+        reader = Morus_Reader()
+        reader.start(2338, 32775, self)
+      
+
+if __name__ == '__main__':
+    app = tornado.web.Application([
+      (r'/', ScaleSocketHandler),     
+    ])
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+    
+
+    
+    
