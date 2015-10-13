e8443af0a5faa746e7100b67c91fe0d019a6d57f
Dmitry White <white.dmitry@gmail.com>
2015-10-13 12:03:46 -0700

    SW-001 Basically functional Python USB scale reader

--- exec/py_device_reader.py
+++ exec/py_device_reader.py
@@ -1,13 +1,16 @@
 import pywinusb.hid as HID
 
-class Morus_Reader:
+class Morus_Reader():
   suspended = False
+
+  tare = []
 
   scale = False
 
   messages = {
-    CONNECTED: 'Scale is connected',
-    NOT_CONNECTED: 'Could not find anticipated scale'
+    'CONNECTED': 'Scale is connected',
+    'NOT_CONNECTED': 'Could not find anticipated scale',
+    'DISCONNECT': 'Scale disconnecting'
   }
 
   def start(self, vendor_id, product_id, socket):
@@ -27,21 +30,33 @@ class Morus_Reader:
     if not self.suspended:
       self.suspended = True
 
-  def tell(data):
+  def tell(self, data):
     # broadcast data to the console and the client
+    print(data)
 
   def resume(self):
     if self.suspended:
       self.suspended = False
 
-  def found_scale():
+  def found_scale(self):
     # broadcast the goodnews and begin listening
+    self.tell(self.messages['CONNECTED'])
+    self.scale.open()
+    self.scale.set_raw_data_handler(self.listen_for_weight)
 
-  def couldnt_find_scale():
+  def couldnt_find_scale(self):
     # broadcast the bad news and close the websocket connection
+    print('ugh')
 
-  def listen_for_weight():
+  def listen_for_weight(self, data):
     # listen for weight, and send the message when weight is received
+    if not self.suspended and data != self.tare:
+      self.tare = data
+      self.tell(data)
 
-  def terminate():
+  def terminate(self):
     # broadcast termination message and close the websocket connection
+    print('terminating')
+
+if __name__ == "__main__":
+  Morus_Reader()
