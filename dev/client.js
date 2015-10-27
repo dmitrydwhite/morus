0fc16cab5e115e2e3275b67d985440678db6d9ba
Dmitry White <dmitry.white@nike.com>
2015-10-27 13:12:54 -0700

    Moved client javascript to dev folder, created a minified js file for reference

--- dev/client.js
+++ dev/client.js
@@ -8,35 +8,31 @@
       var scaleWeight = parseInt(realArray[4]) / 10;
       var unitByte = realArray[2];
       var units = '';
-      var Mweight = document.getElementsByClassName('morus-weight')[0]
-      var Munits = document.getElementsByClassName('morus-units')[0]
+      var Mweight = document.getElementsByClassName('morus-weight')[0];
+      var Munits = document.getElementsByClassName('morus-units')[0];
 
       if (unitByte === 12) {
         units = 'lbs.';
       } else if (unitByte === 3) {
         units = 'kg.';        
       } else {
-        units = 'NA'
+        units = 'NA';
       }
 
-      if (scaleWeight == NaN) {
-        scaleWeight = 'ØØ'
+      if (isNan(scaleWeight)) {
+        scaleWeight = 'Ee';
       }
-      
-      // $('.morus-weight').text(scaleWeight);
-      // $('.morus-units').text(units);
+
       if (Mweight) {
-        Mweight.value = scaleWeight
-        Mweight.innerHTML = scaleWeight
+        Mweight.value = scaleWeight;
+        Mweight.innerHTML = scaleWeight;
       }
 
       if (Munits) {
-        Munits.value = units
-        Munits.innerHTML = units
+        Munits.value = units;
+        Munits.innerHTML = units;
       }
     };
 
-  } else {
-    console.log('Not Available To Connect');
   }
 })();
