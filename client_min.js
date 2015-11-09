f233f6002be5e9ea14a4921234968348344c7744
Dmitry White <dmitry.white@nike.com>
2015-11-09 08:50:01 -0800

    Debugging for weight computation

--- client_min.js
+++ client_min.js
@@ -1,1 +1,39 @@
-!function(){Window&&WebSocket&&!Window.morus&&(Window.morus=new WebSocket("ws://localhost:3000/"),Window.morus.onmessage=function(e){var n=e?JSON.parse(e.data):[],s=parseInt(n[5]),o=parseInt(n[4])/10+25.4*s,t=n[2],a="",m=document.getElementsByClassName("morus-weight")[0],u=document.getElementsByClassName("morus-units")[0];a=12===t?"lbs.":3===t?"kg.":"NA",0/0===o&&(o="Ee"),m&&(m.value=o,m.innerHTML=o),u&&(u.value=a,u.innerHTML=a)})}();
+(function () {
+  if (Window && WebSocket && !Window.morus) {
+
+    Window.morus = new WebSocket('ws://localhost:3000/');
+
+    Window.morus.onmessage = function (data) {
+      var realArray = data ? JSON.parse(data.data) : [];
+      var twentyFives = parseInt(realArray[5]);
+      var scaleWeight = (parseInt(realArray[4]) / 10) + (twentyFives * 25);
+      var unitByte = realArray[2];
+      var units = '';
+      var Mweight = document.getElementsByClassName('morus-weight')[0];
+      var Munits = document.getElementsByClassName('morus-units')[0];
+
+      if (unitByte === 12) {
+        units = 'lbs.';
+      } else if (unitByte === 3) {
+        units = 'kg.';        
+      } else {
+        units = 'NA';
+      }
+
+      if (scaleWeight === NaN) {
+        scaleWeight = 'Ee';
+      }
+
+      if (Mweight) {
+        Mweight.value = scaleWeight;
+        Mweight.innerHTML = scaleWeight;
+      }
+
+      if (Munits) {
+        Munits.value = units;
+        Munits.innerHTML = units;
+      }
+    };
+
+  }
+})();
